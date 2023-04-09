87d4e8358bb5217d0e5dc3ec22880bb2c1e1ea7c
Shuma-Yamamoto <s.yamamoto.eng@gmail.com>
2023-04-09 15:29:01 +0900

    fix: 不要と思われるコードの削除

--- app.py
+++ app.py
@@ -102,6 +102,3 @@ def api():
         file.save(file.filename)
         top_10_json = word_ranking(file)
         return top_10_json
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
